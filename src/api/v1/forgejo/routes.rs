89cd536e6ec55fda18a1301db9a7ad0ff1504a88
Gergely Nagy <me@gergo.csillger.hu>
2023-10-28 19:11:08 +0200

    feat(forgejo): introduce self-hosted forgejo api endpoint
    This introduces the `/v1/forgejo/:host/:user/:repo/` routes, where
    `:host` is any forgejo (or gitea, or any other compatible forge)
    instance.
    
    Signed-off-by: Gergely Nagy <me@gergo.csillger.hu>

---
+++ src/api/v1/forgejo/routes.rs
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2023 Gergely Nagy
+// SPDX-FileContributor: Gergely Nagy
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use super::endpoints::{get_repo, get_repo_ref};
+
+use axum::{routing::get, Router};
+
+pub fn get_routes() -> Router {
+    Router::new()
+        .route("/v1/forgejo/:host/:user/:repo/b/:branch", get(get_repo_ref))
+        .route(
+            "/v1/forgejo/:host/:user/:repo/branch/:branch",
+            get(get_repo_ref),
+        )
+        .route(
+            "/v1/forgejo/:host/:user/:repo/v/:version",
+            get(get_repo_ref),
+        )
+        .route(
+            "/v1/forgejo/:host/:user/:repo/version/:version",
+            get(get_repo_ref),
+        )
+        .route(
+            "/v1/forgejo/:host/:user/:repo/t/:version",
+            get(get_repo_ref),
+        )
+        .route(
+            "/v1/forgejo/:host/:user/:repo/tag/:version",
+            get(get_repo_ref),
+        )
+        .route("/v1/forgejo/:host/:user/:repo", get(get_repo))
+}
