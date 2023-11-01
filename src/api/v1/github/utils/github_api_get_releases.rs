e0b78c31cd6c6060a0489b2b168afcfe74167412
Gergely Nagy <me@gergo.csillger.hu>
2023-11-01 01:11:45 +0100

    refactor(releases): Lift out the release list fetching
    All of the forges Rime currently supports have a very similar way to
    query the releases of a particular repository, the major difference - as
    far as Rime is concerned - is the URL. This is clearly shown by all the
    `forge_api_get_releases()` functions essentially being copy pastes of
    each other.
    
    To reduce the amount of copy paste, lift that shared code out into
    `ForgeReleases::from_url()`, with the previous helper functions
    remaining as thin wrappers around it.
    
    Signed-off-by: Gergely Nagy <me@gergo.csillger.hu>

--- src/api/v1/github/utils/github_api_get_releases.rs
+++ src/api/v1/github/utils/github_api_get_releases.rs
@@ -5,6 +5,7 @@
 
 #[allow(unused)]
 use log::{debug, error, info, trace, warn};
+use reqwest::Url;
 
 use super::super::super::utils::ForgeReleases;
 
@@ -13,35 +14,9 @@ pub async fn github_api_get_releases(
     user: String,
     repo: String,
 ) -> Result<ForgeReleases, Box<dyn std::error::Error + Send + Sync>> {
-    use reqwest::{
-        header::{ACCEPT, USER_AGENT},
-        Url,
-    };
-    let version_uri = Url::parse(&format!(
+    let releases_uri = Url::parse(&format!(
         "http://api.github.com/repos/{}/{}/releases?per_page={}",
         user, repo, page_size
     ))?;
-    trace!("{:#?}", version_uri);
-    let client = reqwest::Client::builder().user_agent(USER_AGENT).build()?;
-    let res = client
-        .get(version_uri)
-        .header(ACCEPT, "application/vnd.github+json")
-        .header("X-GitHub-Api-Version", "2022-11-28")
-        .send()
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-
-    let releases = if res.is_array() {
-        ForgeReleases::from(
-            res.as_array()
-                .expect("Failed to unwrap releases API response as_array()")
-                .iter(),
-        )
-    } else {
-        ForgeReleases::new()
-    };
-    trace!("releases: {releases:#?}");
-
-    Ok(releases)
+    ForgeReleases::from_url(releases_uri).await
 }
